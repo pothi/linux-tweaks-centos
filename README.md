7dbcbed2a6c200c58b8f51c9395d1acab823eeef
Pothi Kalimuthu <pothi@tinywp.com>
2015-02-20 10:38:17 +0530

    Create bootstrap script

--- README.md
+++ README.md
@@ -5,45 +5,16 @@ Tweaks on bash, zsh, vim, tmux, etc on CentOS
 
 ## Install procedure
 
-```bash
-# as root
-# yum install zsh zsh-html -y
+mkdir ~/scripts
+curl -Sso ~/scripts/bootstrap_lt.sh https://raw.githubusercontent.com/pothi/linux-tweaks-centos/master/bootstrap.sh
+chmod +x ~/scripts/bootstrap_lt.sh
 
-# take a backup
-cp -a /etc ~/backups/etc-$(date +%F)-linux-tweaks
+# go through the script to understand what it does. you are warned!
+# vi ~/scripts/bootstrap_lt.sh
 
-# get the source from Github
-cd $HOME && git clone --recursive git://github.com/pothi/linux-tweaks-centos.git
+# run it and face the consequences
+~/scripts/bootstrap_lt.sh
 
-# Shell related configs
-cd ~/linux-tweaks; cp tiny_* /etc/profile.d/
-cat zprofile > /etc/zprofile && cat zshrc > /etc/zshrc
-
-# Vim related configs
-cat vimrc.local > /etc/vim/vimrc.local && cp -a vim/* /usr/share/vim/vimcurrent/
-
-# Common for all users
-touch /etc/skel/.viminfo
-echo 'HISTFILE=~/log/zsh_history' >> /etc/skel/.zshrc
-echo 'export EDITOR=vim' >> /etc/skel/.zshrc
-echo 'export VISUAL=vim' >> /etc/skel/.zshrc
-
-echo "set viminfo='10,\"100,:20,%,n~/log/viminfo" >> /etc/skel/.vimrc
-
-# Misc files
-cat tmux.conf > /etc/tmux.conf && cat gitconfig > /etc/gitconfig
-
-# Clean up
-cd $HOME; rm -rf linux-tweaks/
-
-# Change Shell
-chsh --shell /bin/zsh
-chsh --shell /bin/zsh pothi
-# chsh --shell /bin/zsh sftpuser
-```
-
-#### Update Pathogen (optional)
-```bash
-curl -Sso /usr/share/vim/vimcurrent/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-```
+# get rid of all the evidences
+rm ~/scripts/bootstrap_lt.sh
+rmdir ~/scripts &> /dev/null
---
+++ bootstrap.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Tweaks on bash, zsh, vim, tmux, etc on CentOS
+
+# as root
+if [[ $USER != "root" ]]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+# install dependencies
+yum install zsh zsh-html git -y
+
+# take a backup
+LT_DIRECTORY="~/backups/etc-$(date +%F)-linux-tweaks"
+if [ ! -d "$LT_DIRECTORY" ]; then
+	cp -a /etc $LT_DIRECTORY
+fi
+
+# get the source from Github
+rm -rf ~/lt
+git clone --recursive https://github.com/pothi/linux-tweaks-centos.git ~/lt
+
+# Shell related configs
+cp ~/lt/tiny_* /etc/profile.d/
+cat ~/lt/zprofile > /etc/zprofile
+cat ~/lt/zshrc > /etc/zshrc
+
+# Vim related configs
+cat ~/lt/vimrc.local > /etc/vim/vimrc.local
+# cp -a ~/lt/vim/* /usr/share/vim/vimcurrent/
+cp -a ~/lt/vim/* /usr/share/vim/vim74/
+
+# Common for all users
+touch /etc/skel/.viminfo
+echo 'HISTFILE=~/log/zsh_history' >> /etc/skel/.zshrc
+echo 'export EDITOR=vim' >> /etc/skel/.zshrc
+echo 'export VISUAL=vim' >> /etc/skel/.zshrc
+
+echo "set viminfo='10,\"100,:20,%,n~/log/viminfo" >> /etc/skel/.vimrc
+
+# For root
+cp /etc/skel/.zshrc ~/
+cp /etc/skel/.vimrc ~/
+touch ~/.viminfo
+
+# Misc files
+cat ~/lt/tmux.conf > /etc/tmux.conf
+cat ~/lt/gitconfig > /etc/gitconfig
+
+# Clean up
+rm -rf ~/lt/
+
+# Change Shell
+chsh --shell /bin/zsh
+# chsh --shell /bin/zsh pothi
+# chsh --shell /bin/zsh client
+
+#### Update Pathogen (optional)
+# curl -Sso /usr/share/vim/vimcurrent/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl -Sso /usr/share/vim/vim74/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# logout and then login to see the changes
