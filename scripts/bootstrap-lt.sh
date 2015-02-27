2f7dfc1a19bb0cea352c58f5ca92b8f6996ed973
Pothi Kalimuthu <pothi.rti@gmail.com>
2015-02-27 09:50:06 +0530

    Remove unwanted directories; better naming scheme

--- scripts/bootstrap-lt.sh
+++ scripts/bootstrap-lt.sh
@@ -13,27 +13,26 @@ echo 'Installing dependencies...'
 yum update -y -q
 yum install zsh zsh-html git vim -y -q
 
-mkdir ~/backups &> /dev/null
-mkdir -p ~/backups/{files,databases} ~/{log,tmp,others,scripts} &> /dev/null
+mkdir ~/{backups,log,tmp,others,scripts} &> /dev/null
 
 # take a backup
-LT_DIRECTORY="/root/backups/etc-$(date +%F)-linux-tweaks"
+LT_DIRECTORY="/root/backups/etc-$(date +%F)-linux-tweaks-centos"
 if [ ! -d "$LT_DIRECTORY" ]; then
 	cp -a /etc $LT_DIRECTORY
 fi
 
 # get the source from Github
-rm -rf ~/lt
-git clone --recursive https://github.com/pothi/linux-tweaks-centos.git ~/lt
+rm -rf ~/ltc
+git clone --recursive https://github.com/pothi/linux-tweaks-centos.git ~/ltc
 
 # Shell related configs
-cp ~/lt/tiny_* /etc/profile.d/
-cat ~/lt/zprofile > /etc/zprofile
-cat ~/lt/zshrc > /etc/zshrc
+cp ~/ltc/tiny_* /etc/profile.d/
+cat ~/ltc/zprofile > /etc/zprofile
+cat ~/ltc/zshrc > /etc/zshrc
 
 # Vim related configs
-cat ~/lt/vimrc.local > /etc/vimrc
-cp -a ~/lt/vim/* /usr/share/vim/vim74/
+cat ~/ltc/vimrc.local > /etc/vimrc
+cp -a ~/ltc/vim/* /usr/share/vim/vim74/
 
 # Common for all users
 touch /etc/skel/.viminfo
@@ -49,16 +48,14 @@ cp /etc/skel/.vimrc ~/
 touch ~/.viminfo
 
 # Misc files
-cat ~/lt/tmux.conf > /etc/tmux.conf
-cat ~/lt/gitconfig > /etc/gitconfig
+cat ~/ltc/tmux.conf > /etc/tmux.conf
+cat ~/ltc/gitconfig > /etc/gitconfig
 
 # Clean up
-rm -rf ~/lt/
+rm -rf ~/ltc/
 
 # Change Shell
 chsh --shell /bin/zsh
-# chsh --shell /bin/zsh pothi
-# chsh --shell /bin/zsh client
 
 #### Update Pathogen (optional)
 curl -Sso /usr/share/vim/vim74/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
